1efb96c50ded93f5ff82145282a9c7c3c76d3c8d
Mateus Paixão <mateus.borges.paixao@gmail.com>
2024-07-16 23:19:52 -0300

    first commit :tada:

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+// alert("Funcionou!")
+
+document.write("Abra o devTools para visualizar o que está feito!");
+
+console.log("Funcionou!");
+console.error("Não Funcionou!");
+console.warn("Presta atenção nisso!");
+
+
+var brinquedo = "boneca"; // entre aspas ("") é uma string
+
+console.log(brinquedo);
+brinquedo = "carrinho";
+console.log(brinquedo);
+brinquedo = "20";
+console.log(typeof brinquedo);
+console.log(brinquedo);
+
+console.clear();
+var numero = 20.4;
+console.log(typeof numero);
+
+var logico = true; // false 
+console.log(logico);
+console.log(typeof logico);
+
+var naoAtribuido; // Undefined
+console.log(naoAtribuido);
+console.log(typeof naoAtribuido);
+
+var valorNull = null;
+console.log(valorNull); 
+// console.log(typeof valorNull); // TODO: falar sobre Object
+
+// camelCase, firstName, lastNameAndFathersName
+// PascalCase
+// snake_case
+// kebab-case
+
+// Tipos primitivos
+// String
+// Number
+// Boolean
+// Undefined
+// Null
